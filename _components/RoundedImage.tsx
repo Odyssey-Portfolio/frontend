0a680fcc1188547d417b0e2777b0c431fee6d3b5
vathuglife <thuandt582@gmail.com>
2025-02-09 10:26:30 +0700

    dva: Added Welcome section for Home page

---
+++ _components/RoundedImage.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+export interface RoundedImageProps {
+  src: string;
+  width: number;
+  height: number;
+}
+export default function RoundedImage(props: RoundedImageProps) {
+  const avatarClassname = `w-4/5 h-4/5 rounded-full overflow-hidden`;
+  return (
+    <Image
+      className={avatarClassname}
+      src={props.src}
+      alt="avatar"
+      width={props.width}
+      height={props.height}
+    />
+  );
+}
---
+++ _constants/Fonts.ts
@@ -0,0 +1,4 @@
+import { Lexend, Poppins } from "next/font/google";
+
+export const FONT_LEXEND = Lexend()
+export const FONT_POPPINS = Poppins({weight:"400"})
