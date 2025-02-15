813d5bfc94a62d7260851be856a764cb4bc7c024
vathuglife <thuandt582@gmail.com>
2025-02-15 10:18:51 +0700

    dva: Added year indicator to scrollableTimeline

--- _components/ScrollableTimeline.tsx
+++ _components/ScrollableTimeline.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { COLOR_PRIMARY, COLOR_SECONDARY } from "@/_constants/Colors";
+import {
+  COLOR_PRIMARY,
+  COLOR_SECONDARY,
+  COLOR_WHITE,
+} from "@/_constants/Colors";
+import { FONT_LEXEND, FONTSTYLE_SUBTEXT2 } from "@/_constants/Fonts";
 import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 import useScrollPercentage from "../_hooks/useScrollPercentage";
@@ -10,10 +15,10 @@ export default function ScrollableTimeline() {
     visible: { opacity: 1, x: 0 },
   };
   const scrollPercentage = useScrollPercentage();
-  console.log("Current scroll percentage: ", scrollPercentage);
-  const timelineBgClassname = `flex flex-col-reverse relative 
+  const wrapperClassname = "flex flex-row h-96";
+  const scrollableTimelineClassname = `flex flex-col-reverse relative 
                                 rounded-3xl bg-white
-                                border-2 w-8 h-96`;
+                                border-2 w-8`;
   const innerTimelineClassname = `w-full rounded-3xl flex-reverse`;
   const [isVisible, setIsVisible] = useState(false);
   let hideTimeout: NodeJS.Timeout;
@@ -38,20 +43,56 @@ export default function ScrollableTimeline() {
     <motion.div
       variants={variants}
       initial="hidden"
-      animate={isVisible ? "visible" : "hidden"}
+      animate={isVisible ? "visible" : "visible"}
     >
-      <div
-        className={timelineBgClassname}
-        style={{ borderColor: COLOR_PRIMARY }}
-      >
+      <div className={wrapperClassname}>
+        <YearIndicator scrollPercentage={scrollPercentage} />
         <div
-          className={innerTimelineClassname}
-          style={{
-            height: `${100 - scrollPercentage}%`,
-            backgroundColor: COLOR_SECONDARY,
-          }}
-        />
+          className={scrollableTimelineClassname}
+          style={{ borderColor: COLOR_PRIMARY }}
+        >
+          <div
+            className={innerTimelineClassname}
+            style={{
+              height: `${100 - scrollPercentage}%`,
+              backgroundColor: COLOR_SECONDARY,
+            }}
+          />
+        </div>
       </div>
     </motion.div>
+  );
+}
+
+interface YearIndicatorProps {
+  scrollPercentage: number;
+}
+function YearIndicator(props: YearIndicatorProps) {
+  const yearIndicatorWrapperClassname = "relative flex flex-row h-12";
+  const yearIndicatorClassname = `rounded-l-3xl p-5 ${FONT_LEXEND.className} 
+                                  ${FONTSTYLE_SUBTEXT2}
+                                  flex flex-row items-center`;
+  return (
+    <div
+      className={yearIndicatorWrapperClassname}
+      style={{ top: `${props.scrollPercentage}%` }}
+    >
+      <div
+        className={yearIndicatorClassname}
+        style={{
+          color: COLOR_WHITE,
+          backgroundColor: COLOR_SECONDARY,
+        }}
+      >
+        2024
+      </div>
+      <div
+        className={`w-12`}
+        style={{
+          clipPath: `polygon(0 0, 50% 50%, 0 100%)`,
+          backgroundColor: COLOR_SECONDARY,
+        }}
+      />
+    </div>
   );
 }
