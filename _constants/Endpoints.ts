25a3a26f7fac495a7788e0488699ded53e3e8859
vathuglife <thuandt582@gmail.com>
2025-05-21 21:12:07 +0700

    dva: Updated deployment server

--- _constants/Endpoints.ts
+++ _constants/Endpoints.ts
@@ -1,8 +1,10 @@
 export const LOCAL_SERVER_URL = "http://localhost:5018";
 export const LOCAL_SECURE_SERVER_URL = "https://localhost:7206";
+export const DEPLOYED_SERVER_URL =
+  "https://odyssey-portfolio-backend.onrender.com";
 
-export const BLOG_ENDPOINT = `${LOCAL_SECURE_SERVER_URL}/blog`;
+export const BLOG_ENDPOINT = `${DEPLOYED_SERVER_URL}/blog`;
 
 /*AUTHENTICATION ENDPOINTS*/
-export const LOGIN_ENDPOINT = `${LOCAL_SECURE_SERVER_URL}/user/login`;
-export const REGISTER_ENDPOINT = `${LOCAL_SECURE_SERVER_URL}/user/register`;
+export const LOGIN_ENDPOINT = `${DEPLOYED_SERVER_URL}/user/login`;
+export const REGISTER_ENDPOINT = `${DEPLOYED_SERVER_URL}/user/register`;
