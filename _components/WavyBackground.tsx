a55bc80635b1298e845bb1218139704b98a4b47d
vathuglife <thuandt582@gmail.com>
2025-05-23 07:47:19 +0700

    dva: Added global background for application

---
+++ _components/WavyBackground.tsx
@@ -0,0 +1,45 @@
+interface WavyBackgroundProps {
+  children: React.ReactNode;
+}
+export default function WavyBackground(props: WavyBackgroundProps) {
+  return (
+    <>
+      <div className="relative w-full min-h-screen">
+        <Background />
+        {/* Page content */}
+        <div className="relative z-10">{props.children}</div>
+      </div>
+    </>
+  );
+}
+
+function Background() {
+  const waveColors = ["#0066cc", "#3399ff", "#66ccff", "#99ddff", "#cceeff"];
+
+  // 2 full wave crests: Start -> Down -> Up -> Down again
+  const wavePath =
+    "M0,160 " +
+    "C360,320, 720,0, 1080,160 " + // Wave 1 (down -> up)
+    "C1260,213, 1380,213, 1440,160 " + // Wave 2 (gentle dip)
+    "L1440,320 L0,320 Z"; // Bottom close
+
+  return (
+    <div className="fixed inset-[-10] -z-10">
+      {waveColors.map((fill, i) => (
+        <div
+          key={i}
+          className="absolute inset-0"
+          style={{ top: `${i * 10}%`, height: "100vh" }}
+        >
+          <svg
+            viewBox="0 0 1440 320"
+            className="w-full h-full"
+            preserveAspectRatio="none"
+          >
+            <path d={wavePath} fill={fill} fillOpacity={0.05} />
+          </svg>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import NavBar from "@/_components/NavBar";
 import SnackbarList from "@/_components/Snackbars";
+import WavyBackground from "@/_components/WavyBackground";
 import { Providers } from "@/_redux/Providers";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Odyssey Portfolio",
@@ -30,19 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const navbarClassname = "flex flex-row justify-center";
+  const backgroundClassname = `relative w-full min-h-screen`;
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/airplane.ico" type="image/png" />
       </head>
       <Providers>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${backgroundClassname} antialiased`}>
           <div className={navbarClassname}>
             <NavBar />
           </div>
-          {children}
+          <WavyBackground>{children}</WavyBackground>
           <SnackbarList />
         </body>
       </Providers>
